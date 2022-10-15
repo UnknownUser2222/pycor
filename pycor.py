aa92609162af017449860efc752591ad132d757b
UnknownUser2222 <110921504+UnknownUser2222@users.noreply.github.com>
2022-10-15 12:26:48 +0600

    Update files

--- pycor.py
+++ pycor.py
@@ -1,4 +1,4 @@
-print("Requires Python 3.10.0 or later.")
+print("Requires Python 3.10.8 or later.")
 print("Welcome to the PyCor v1.01.0")
 print("Type help_pycor for help.")
 x = ""
@@ -14,6 +14,30 @@ while x != "0":
         print("add_script - Adds a Python script")
         print("remove_object - Removes an object.")
         print("remove_python_key - Deletes a Python key.")
+        print("remove_key - Deletes a key")
+        print("remove_script - Removes a script.")
+        print("add_python_object - Adds a Python object.")
+        print("add_pycor_variable - Adds a PyCor variable.")
+        print("add_jscript_variable - Adds a JavaScript variable")
+        print("remove_python_object - Removes a Python object")
+        print("client_redirect_host - Redirects to host server.")
+        print("connect_to_github_server - Connects to a GitHub repository.")
+        print("More coming soon in v1.02.0!")
+    elif x == str("connect_to_github_server"):
+        input("Name of repo: ")
+        input("Owner or organization: ")
+        print("Successfully joined server.")
+    elif x == str("client_redirect_host"):
+        input("Name of server: ")
+        input("Directory: ")
+        print("Successfully joined server.")
+    elif x == str("remove_python_object"):
+        input("Name: ")
+        print("Successfully removed object.")
+    elif x == str("add_jscript_variable"):
+        input("Name: ")
+        input("Value: ")
+        print("Successfully added JScript object")
     elif x == str("add_script"):
         input("Function name: ")
         input("Function code: {__one_line_script: ")
@@ -21,6 +45,22 @@ while x != "0":
     elif x == str("remove_object"):
         input("Name: ")
         print("Successfully removed object")
+
+    elif x == str("remove_script"):
+        input("Name: ")
+        print("Successfully removed script.")
+
+    elif x == str("add_python_object"):
+        input("Name: ")
+        input("Value: ")
+        print("Successfully added object")
+
+    elif x == str("add_pycor_variable"):
+        input("Name: ")
+        print("Successfully added variable")
+    elif x == str("remove_key"):
+        input("Name: ")
+        print("Successfully removed key")
     elif x == str("add_python_key"):
         print("Python keys can be useful if an 'object' does not work.")
         input("Name: ")
@@ -34,10 +74,10 @@ while x != "0":
     elif x == str("add_key"):
         input1 = input("WARNING: Adding keys without a reason might require a reinstall. Are you sure do you want to continue? ([Y]es or [N]o)")
         if input1 == "Y":
-           key_name =  input("Key name: ")
-           key_type = input("Key type: ")
-           key_value = input("Key value: ")
-           print("Key " + key_name + " with type " + key_type + " as " + key_value + " successfully added to registry")
+            key_name = input("Key name: ")
+            key_type = input("Key type: ")
+            key_value = input("Key value: ")
+            print("Key " + key_name + " with type " + key_type + " as " + key_value + " successfully added to registry")
     elif x == str("add_object"):
 
         cmd2_1 = input("Name: ")
@@ -45,8 +85,7 @@ while x != "0":
         print("Successfully added to dictionary")
         print(cmd1)
     elif x == str(""):
-     False
+        key = True
+
     else:
         print("This term is not a command or a script. Please check if you have any typos in the scripts you typed.")
-
-
