562f5756b77893dedfff9a974d5389ae1c138006
UnknownUser2222 <userunamed981@gmail.com>
2022-11-03 13:12:10 +0600

    Delete cmd_config.js

--- cmd_config.js
+++
@@ -1,8 +0,0 @@
-var cmds = require("cmds")
-cmds.spc = "374858246"
-cmds.location = "releases/format@all.with/PyCorSetup.exe"
-cmds.inc = "robots"
-const pythonProject = "PyCharmProjects/pythonproject"
-pythonProject.alr = {
-"cmds": "kbs"
-}
